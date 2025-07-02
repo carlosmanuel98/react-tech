ab483feed85b2f4ea71c1b698180694d365fd8e9
Carlos Manuel Rojas Leiba <cmrojas@provincianet.com.ar>
2025-07-02 20:33:44 -0300

    avance entrega final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,27 +23,28 @@ const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
               <Nav.Link as={Link} to="/crud">CRUD</Nav.Link>
             }
           </Nav>
-          {(!isAuthenticated) ? (
-            <Button variant="outline-light" as={Link} to="/login" className="me-2">Login</Button>
-          ) : (
-            
-            <Nav className="d-flex align-items-center gap-2">
-            <span className="text-white me-2">Hola, {username}</span>
+          <div className="d-flex align-items-center gap-3">
+            {isAuthenticated && (
+              <span className="text-white">Hola, {username}</span>
+            )}
+
             <Button variant="outline-light">
-              <Link to="/cart" className="text-white position-relative">
-                  <FontAwesomeIcon icon={faShoppingCart} size="lg" />
-                  {totalItems > 0 && (
-                    <Badge pill bg="danger" className="position-absolute top-0 start-100 translate-middle">
-                      {totalItems}
-                    </Badge>
-                  )}
-                </Link>
+              <Link to="/cart" className="text-white position-relative text-decoration-none">
+                <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+                {totalItems > 0 && (
+                  <Badge pill bg="danger" className="position-absolute top-0 start-100 translate-middle">
+                    {totalItems}
+                  </Badge>
+                )}
+              </Link>
             </Button>
-            <Button variant="outline-light" onClick={logout}>
-              Cerrar sesión
-            </Button>
-          </Nav>
-          )}
+
+            {!isAuthenticated ? (
+              <Button variant="outline-light" as={Link} to="/login">Login</Button>
+            ) : (
+              <Button variant="outline-light" onClick={logout}>Cerrar sesión</Button>
+            )}
+          </div>
         </Navbar.Collapse>   
       </Container>
     </Navbar>
